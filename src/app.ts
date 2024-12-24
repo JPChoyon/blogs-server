4507133eb4be8f9d1e5f7a5961e19e37b48164ac
JPChoyon <kotjpckhan@gmail.com>
2024-12-24 12:16:14 +0600

    app error and validator are added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import router from './app/router';
 const app = express();
 
 // parser / middleware
@@ -7,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-// app.use('/api/v1', router);
+app.use('/api', router);
 
 const getAController = async (req: Request, res: Response) => {
   // Promise.reject();
